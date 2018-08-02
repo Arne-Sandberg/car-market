0b6bc2704eb7e7154adcedd51a8532c33e723daa
MikeHoangg <hoang.viet.ha.business@gmail.com>
2018-08-02 16:36:27 +0300

    added car brands on home page

--- Market/urls.py
+++ Market/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
+from MarketApp import views
 
 urlpatterns = [
     url('^accounts/', include('registration.backends.simple.urls')),
-    url('(^home$|^$)', TemplateView.as_view(template_name='index.html')),
+    url('(^home$|^$)', views.IndexView.as_view(template_name='index.html')),
 ]
--- MarketApp/views.py
+++ MarketApp/views.py
@@ -1,1 +1,12 @@
+from django.views.generic import TemplateView
+from MarketApp import models
+from django.shortcuts import render
+
+
 # Create your views here.
+
+class IndexView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['data'] = models.Brand.objects.all()
+        return context
