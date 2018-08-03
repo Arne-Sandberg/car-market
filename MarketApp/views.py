f08ba320a37fbd9c6df1eabd3ec9a3c91c97f819
MikeHoangg <hoang.viet.ha.business@gmail.com>
2018-08-03 16:36:19 +0300

    fixed slider on brands page

--- MarketApp/views.py
+++ MarketApp/views.py
@@ -13,6 +13,7 @@ class IndexView(TemplateView):
 
 
 class BrandView(TemplateView):
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['cars'] = models.Car.objects.filter(brand__name=self.kwargs['brand_name'])
