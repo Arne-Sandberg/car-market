a84577d1b0d6ac8ad5ef3ca2c17149c1035da92c
MikeHoangg <hoang.viet.ha.business@gmail.com>
2018-08-03 20:48:27 +0300

    .

--- Market/urls.py
+++ Market/urls.py
@@ -6,6 +6,6 @@ from MarketApp import views
 urlpatterns = [
     url('^accounts/', include('registration.backends.simple.urls')),
     url('^home$|^$', views.IndexView.as_view(template_name='index.html')),
-    url('^brand/(?P<brand_name>\w+)', views.BrandView.as_view(template_name='brands.html')),
+    url('^brand/(?P<brand_name>\w+)/$', views.BrandView.as_view(template_name='brands.html')),
     url('^admin/', admin.site.urls),
 ]
