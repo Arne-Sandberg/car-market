ab6534c0818d4e1cb08bf8b512dae8ed2db211d8
MikeHoangg <hoang.viet.ha.business@gmail.com>
2018-08-09 15:16:58 +0300

    fixed minor issues

--- Market/urls.py
+++ Market/urls.py
@@ -6,9 +6,9 @@ from MarketApp import views
 urlpatterns = [
     url('^accounts/', include('registration.backends.simple.urls')),
     url('^home$|^$', views.IndexView.as_view()),
-    url('^brand/(?P<brand_id>\w+)/$', views.BrandPageView.as_view()),
-    url('^brand/(?P<brand_id>\w+)/(?P<pk>[-\w]+)/$', views.CarPageView.as_view()),
-    url('^filter/(?P<brand_id>\w+)/', views.BrandContent.as_view()),
+    url('^brand/(?P<brand_id>\d+)/$', views.BrandPageView.as_view()),
+    url('^brand/(?P<brand_id>\d+)/(?P<pk>\d+)/$', views.CarPageView.as_view()),
+    url('^filter/(?P<brand_id>\d+)/', views.BrandContent.as_view()),
     url('^admin/', admin.site.urls),
 ]
 
