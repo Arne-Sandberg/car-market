a37e22e6b46d11b3e89da9d7d5feefea593aea02
MikeHoangg <hoang.viet.ha.business@gmail.com>
2018-08-14 15:20:14 +0300

    fixed minor issues

--- MarketApp/views.py
+++ MarketApp/views.py
@@ -191,7 +191,6 @@ class CommentView(TemplateView):
         context = self.get_context_data()
         if data['flag'] == 'edit':
             initial = {'content': data['content'], 'rating': data['rating']}
-            print(initial)
             context['form'] = forms.CommentForm(initial=initial)
             context['edit'] = True
             context['comment_id'] = data['comment_id']
