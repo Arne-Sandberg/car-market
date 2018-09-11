3702d00c9ef5568bafc177312802c740b210550b
MikeHoangg <hoang.viet.ha.business@gmail.com>
2018-09-11 12:47:28 +0300

    fixed minor issues

--- Market/settings.py
+++ Market/settings.py
@@ -149,6 +149,7 @@ ANGULAR_APP_DIR = os.path.join(BASE_DIR, 'marketclientapp/dist')
 
 STATICFILES_DIRS = [
     os.path.join(ANGULAR_APP_DIR),
+    os.path.join(BASE_DIR, 'marketapp/static')
 ]
 
 MEDIA_URL = '/media/'
